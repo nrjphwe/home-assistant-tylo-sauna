09e5e466101e09f2443216102ae82f69202f4d41
nrjphwe <43763288+nrjphwe@users.noreply.github.com>
2026-07-14 23:41:29 +0200

    Update controller.py

--- custom_components/tylo_sauna/controller.py
+++ custom_components/tylo_sauna/controller.py
@@ -699,7 +699,7 @@ class SaunaController:
         self.start_watchdog()
         self._hass.create_task(self._async_init_sequence())
         # self._hass.create_task(self.async_start_cloud_websocket())  # ← Added 2026-06-03
-        # Cloud WebSocket disabled — all control and telemetry via local UDP 2026-07-14
+        #  Cloud WebSocket disabled — all control and telemetry via local UDP 2026-07-14
         # self._hass.async_create_background_task(
         #    self.async_start_cloud_websocket(),
         #     name="Tylo Sauna Cloud WebSocket"
